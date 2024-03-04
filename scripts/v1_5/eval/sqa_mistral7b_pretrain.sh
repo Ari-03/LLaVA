5f218f26cf4fb68578dba4570ce90967cefe9e53
Ge Gao <91497656+gaoge0305@users.noreply.github.com>
2024-03-04 14:35:42 -0800

    Create sqa_mistral7b_pretrain.sh

---
+++ scripts/v1_5/eval/sqa_mistral7b_pretrain.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+python -m llava.eval.model_vqa_science \
+    --model-path /home/haozhang/axie/capstone/aritra/LLaVA/checkpoints/llava-mistral-7b-pretrain \
+    --question-file ./playground/data/eval/scienceqa/llava_test_CQM-A.json \
+    --image-folder ./playground/data/eval/scienceqa/images/test \
+    --answers-file ./playground/data/eval/scienceqa/answers/llava-mistral-7b-pretrain.jsonl \
+    --single-pred-prompt \
+    --temperature 0 \
+    #--conv-mode vicuna_v1
+
+python llava/eval/eval_science_qa.py \
+    --base-dir ./playground/data/eval/scienceqa \
+    --result-file ./playground/data/eval/scienceqa/answers/llava-mistral-7b-pretrain.jsonl \
+    --output-file ./playground/data/eval/scienceqa/answers/llava-mistral-7b-pretrain_output.jsonl \
+    --output-result ./playground/data/eval/scienceqa/answers/llava-mistral-7b-pretrain_result.json
