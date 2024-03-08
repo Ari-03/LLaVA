a66ba154147e410e34751625552bf78ed27b3de7
Aritra Das <64171580+Ari-03@users.noreply.github.com>
2024-03-07 19:34:59 -0800

    Update finetune_lora.sh

--- scripts/Mistral/finetune_lora.sh
+++ scripts/Mistral/finetune_lora.sh
@@ -22,8 +22,8 @@ deepspeed llava/train/train-flash_attention_2.py \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
-    --save_strategy "steps" \
-    --save_steps 1000 \
+    --save_strategy "epochs" \
+    --save_steps 1 \
     --save_total_limit 11 \
     --learning_rate 2e-5 \
     --weight_decay 0. \
