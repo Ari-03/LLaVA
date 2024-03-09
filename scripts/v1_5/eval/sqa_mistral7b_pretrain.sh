c4162f3ebee0bb6c1f167a56a8eab4a17ebb2de4
Ge Gao <91497656+gaoge0305@users.noreply.github.com>
2024-03-08 16:45:20 -0800

    Update sqa_mistral7b_pretrain.sh

--- scripts/v1_5/eval/sqa_mistral7b_pretrain.sh
+++ scripts/v1_5/eval/sqa_mistral7b_pretrain.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 python -m llava.eval.model_vqa_science \
-    --model-path /home/haozhang/axie/capstone/aritra/LLaVA/checkpoints/llava-mistral-7b-pretrain \
+    --model-path ./checkpoints/llava-mistral-7b-pretrain \
     --question-file ./playground/data/eval/scienceqa/llava_test_CQM-A.json \
     --image-folder ./playground/data/eval/scienceqa/images/test \
     --answers-file ./playground/data/eval/scienceqa/answers/llava-mistral-7b-pretrain.jsonl \
+    --model-base mistralai/Mistral-7B-v0.1 \
     --single-pred-prompt \
-    --temperature 0 \
-    --load-4-bit \
-    #--conv-mode vicuna_v1
+    --temperature 0 \ 
 
 python llava/eval/eval_science_qa.py \
     --base-dir ./playground/data/eval/scienceqa \
