d6bdc980c9908cb5ad0d8e68be47cf344ce0afd5
Mac.Ari <d.aritra@outlook.com>
2024-03-09 22:35:29 -0800

    another change

--- llava/model/builder.py
+++ llava/model/builder.py
@@ -160,7 +160,7 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
         if not vision_tower.is_loaded:
             vision_tower.load_model()
         if device_map != 'auto':
-            vision_tower.to(device=device_map, dtype=torch.float16)
+            vision_tower.to(device=device, dtype=torch.float16)
         image_processor = vision_tower.image_processor
 
     if hasattr(model.config, "max_sequence_length"):
