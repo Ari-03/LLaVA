4228baf4e534ae27e4545bcf85324de12da6efe5
Mac.Ari <d.aritra@outlook.com>
2024-03-10 22:41:59 -0700

    another one

--- scripts/Mistral/last_finetune_lora.sh
+++ scripts/Mistral/last_finetune_lora.sh
@@ -13,7 +13,7 @@ deepspeed llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/llava-mistral-7b-task-lora \
+    --output_dir ./checkpoints/llava-mistral-7b-task-lora-v3 \
     --num_train_epochs 5 \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 4 \
